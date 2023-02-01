61d8a31f5520690b24b9dece4de878ee8021dd79
Chijioke Ibekwe <chijioke.ibekwe@sabiroad.com>
2023-02-01 07:28:54 +0100

    Refactor: rename migration files and update db config

--- migrations/sqls/20230130134422-books-table-up.sql
+++
@@ -1,2 +0,0 @@
-/* Replace with your SQL commands */
-CREATE TABLE books (id SERIAL PRIMARY KEY, title VARCHAR(100), author VARCHAR(100), total_pages INTEGER, type VARCHAR(100), summary VARCHAR(100));
